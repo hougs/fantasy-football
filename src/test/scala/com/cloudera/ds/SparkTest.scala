475fb6dba46f9c4d684266e8d3bc55374b797de4
Juliet Hougland <juliet@cloudera.com>
2015-01-28 16:28:36 -0800

    Test play stat generation.

---
+++ src/test/scala/com/cloudera/ds/SparkTest.scala
@@ -0,0 +1,36 @@
+package com.cloudera.ds
+
+import org.apache.spark.SparkContext
+import org.scalatest.FunSuite
+
+object SparkTest extends org.scalatest.Tag("com.qf.test.tags.SparkTest")
+
+trait SparkTestUtils extends FunSuite {
+  var sc: SparkContext = _
+
+  /**
+   * convenience method for tests that use spark.  Creates a local spark context, and cleans
+   * it up even if your test fails.  Also marks the test with the tag SparkTest, so you can
+   * turn it off
+   *
+   * By default, it turn off spark logging, b/c it just clutters up the test output.  However,
+   * when you are actively debugging one test, you may want to turn the logs on
+   *
+   * @param name the name of the test
+   * @param silenceSpark true to turn off spark logging
+   */
+  def sparkTest(name: String, silenceSpark : Boolean = true)(body: => Unit) {
+    test(name, SparkTest){
+      sc = new SparkContext("local[4]", name)
+      try {
+        body
+      }
+      finally {
+        sc.stop
+        sc = null
+        // To avoid Akka rebinding to the same port, since it doesn't unbind immediately on shutdown
+        System.clearProperty("spark.master.port")
+      }
+    }
+  }
+}
--- src/test/scala/com/cloudera/ds/TestPlayerPortfolio.scala
+++ src/test/scala/com/cloudera/ds/TestPlayerPortfolio.scala
@@ -1,17 +1,25 @@
 package scala.com.cloudera.ds
 
-import com.cloudera.ds.{PlayerGameRecord, playerPortfolios}
-import org.apache.spark.SparkContext
-import org.scalatest.FlatSpec
+import com.cloudera.ds._
+import org.scalatest.ShouldMatchers
+import org.apache.spark.SparkContext._
 
-class TestPlayerPortfolio extends FlatSpec {
-  val sc = new SparkContext("local", "Player Portfolio Optimization")
-  val playerGameRdd = sc.parallelize(List(new PlayerGameRecord("foo", 1, 0, 1, 1),
-  new PlayerGameRecord("bar", 2, 0, 2, 1)))
-  val pairs = playerPortfolios.playerGameStats(playerGameRdd)
-  val pairsClass = pairs.getClass
-  println(s"Class of pairs is [$pairsClass]")
-  val elemPairsClass = pairs.first().getClass
-  println(s"Class of elements of pairs is [$elemPairsClass]")
 
+class TestPlayerPortfolio extends SparkTestUtils with ShouldMatchers {
+
+  sparkTest("Test playerSeasonState method") {
+    val playerGameRdd = sc.parallelize(List(new PlayerGameRecord("foo", 1, 0, 1, 1),
+      new PlayerGameRecord("foo", 1, 0, 1, 3),
+      new PlayerGameRecord("bar", 2, 0, 2, 1)))
+    val gameSeasonRdd = sc.parallelize(List((1, 2001L), (2, 2002L)))
+    val pairs: scala.collection.Map[(String, Long), PlayerStats] = playerPortfolios
+      .playerSeasonStats(playerGameRdd, gameSeasonRdd).collectAsMap()
+    pairs should have size 2
+    pairs should contain key (("foo", 2001L))
+    pairs(("foo", 2001L)) should equal(PlayerStats(2L, StatSummary(0, 0), StatSummary(1, 0),
+      StatSummary(2, 2), StatSummary(3, 2)))
+    pairs should contain key (("bar", 2002L))
+    pairs(("bar", 2002L)) should equal(PlayerStats(1L, StatSummary(0, 0), StatSummary(2, 0),
+      StatSummary(1, 0), StatSummary(3, 0)))
+  }
 }
