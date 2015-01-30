6218f190f00cf51c946ae52aa7514b07e2c79988
Juliet Hougland <not@myemail.com>
2015-01-29 21:29:30 -0800

    Adds scrapper for grabbing injury data from a webpage.

---
+++ scripts/InjuryDataScraper.py
@@ -0,0 +1,20 @@
+from BeautifulSoup import BeautifulSoup
+from urllib2 import urlopen
+import csv
+
+soup = BeautifulSoup(urlopen('http://www.cbssports.com/nfl/injuries'))
+
+outputFile = open("injuries.csv", "w")
+csvwriter = csv.writer(outputFile)
+
+for table in soup.findAll("table", attrs = {"class" : "data"}):
+    rows = table.findAll("tr")
+    if len(rows) > 0:
+        rows.pop(0)
+        for row in rows:
+            cells = [cell.text for cell in row.findAll('td')]
+            print cells
+            if len(cells) == 6:
+                csvwriter.writerow(cells)
+
+outputFile.close()
