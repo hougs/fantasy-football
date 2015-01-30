1ae959ddd8a138705a3ca222009c2f954b961067
Juliet Hougland <juliet@cloudera.com>
2015-01-30 14:08:24 -0800

    add hive jars to classpath

--- src/main/scala/com/cloudera/ds/playerPortfolios.scala
+++ src/main/scala/com/cloudera/ds/playerPortfolios.scala
@@ -1,10 +1,11 @@
 package com.cloudera.ds
 
-import org.apache.spark.SparkContext
+import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
 import org.apache.spark.mllib.linalg.Vector
 import org.apache.spark.mllib.stat.MultivariateOnlineSummarizer
 import org.apache.spark.rdd.RDD
+import sys.process._
 
 
 object playerPortfolios {
@@ -30,13 +31,29 @@ object playerPortfolios {
       PlayerStats(statSummarizer)
     }
   }
+
+  def getHiveJars(): Array[String] = {
+    val result: String = "find /opt/cloudera/parcels/CDH-5.3.0-1.cdh5.3.0.p0.30/lib/hive/lib/ " +
+      "-not -name \"guava*\"" !!
+    val jars: Array[String]= result.split("\n")
+    jars
+  }
+
+  def configure(master: String): SparkConf = {
+    val conf = new SparkConf()
+    conf.setMaster(master)
+    conf.setAppName("Player Portfolio Optimization")
+    conf.set("spark.executor.extraClassPath", "")
+    conf
+  }
   /** Entry point to this Spark job. */
   def main(args: Array[String]) {
     val master = args.length match {
       case x: Int if x > 0 => args(0)
       case _ => "local"
     }
-    val sc = new SparkContext(master, "Player Portfolio Optimization")
+
+    val sc = new SparkContext(configure(master))
     val gameSeason = DataSource.gamesSeasonHiveSparkSql(sc)
     val playerGame = DataSource.playerGameRddSparkSql(sc)
 
