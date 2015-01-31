a8b3fd2a11fc3c5331f50596adb2d1d1f1c3bd7c
Juliet Hougland <not@myemail.com>
2015-01-31 10:49:54 -0800

    Fix up InjuryDataScrepper.py

--- scripts/InjuryDataScraper.py
+++ scripts/InjuryDataScraper.py
@@ -8,13 +8,13 @@ outputFile = open("injuries.csv", "w")
 csvwriter = csv.writer(outputFile)
 
 for table in soup.findAll("table", attrs = {"class" : "data"}):
-    team_name = table.find("a").contents
+    team_name = table.find("a")
+    print team_name
     rows = table.findAll("tr")
     if len(rows) > 0:
         rows.pop(0)
         for row in rows:
             cells = [cell.text for cell in row.findAll('td')]
-            print cells
             if len(cells) == 6:
                 cells += team_name
                 csvwriter.writerow(cells)
