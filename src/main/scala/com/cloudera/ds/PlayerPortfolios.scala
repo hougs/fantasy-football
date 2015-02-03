718acd1e7d1829c11b0790faa7bcd408ebc941a0
Juliet Hougland <juliet@cloudera.com>
2015-02-02 17:16:42 -0800

    Add filtering logic to player portfolios

--- src/main/scala/com/cloudera/ds/PlayerPortfolios.scala
+++ src/main/scala/com/cloudera/ds/PlayerPortfolios.scala
@@ -1,5 +1,6 @@
 package com.cloudera.ds
 
+import com.cloudera.ds.football.avro.PlayerStats
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.SparkContext._
 import org.apache.spark.mllib.linalg.Vector
@@ -8,14 +9,14 @@ import org.apache.spark.rdd.RDD
 import sys.process._
 
 
-object playerPortfolios {
+object PlayerPortfolios {
 
   /**
    * Returns an RDD keyed by player id and season pairs, with associated values statistics on
    * fantasy football points.
    */
   def playerSeasonStats(playerGameRecordRdd: RDD[PlayerGameRecord],
-                        gameSeasonRdd: RDD[(Int, Int)]): RDD[((String, Int), PlayerStats)] = {
+                        gameSeasonRdd: RDD[(Int, Int)]): RDD[((String, Int), SingleYearStats)] = {
     // name Rdd with indication of how it is keyed
     val PlayerRecordsByGID = playerGameRecordRdd.map(record => (record.game, record))
     // Elements of this Rdd are (PlayerRecord, Sea)
@@ -24,18 +25,19 @@ object playerPortfolios {
     val myPair: RDD[((String, Int), Vector)] = PlayerRecordSeason.map(element => ((element._1
       .playerId, element._2), element._1.getNumericValsAsVector()))
     val statSummarizer = myPair.aggregateByKey(new MultivariateOnlineSummarizer())((summarizer:
-           MultivariateOnlineSummarizer, value: Vector) => summarizer.add(value),
+                                                                                    MultivariateOnlineSummarizer, value: Vector) => summarizer.add(value),
       (summarizer1: MultivariateOnlineSummarizer, summarizer2: MultivariateOnlineSummarizer) =>
         summarizer1.merge(summarizer2))
     statSummarizer.mapValues { statSummarizer: MultivariateOnlineSummarizer =>
-      PlayerStats(statSummarizer)
+      SingleYearStats(statSummarizer)
     }
   }
 
-  def getHiveJars(): String= {
-    val result: String = "find /opt/cloudera/parcels/CDH-5.3.0-1.cdh5.3.0.p0.30/lib/hive/lib/ -name" +
+  def getHiveJars(): String = {
+    val result: String = "find /opt/cloudera/parcels/CDH-5.3.0-1.cdh5.3.0.p0.30/lib/hive/lib/ " +
+      "-name" +
       " '*.jar' -not -name 'guava*'" !!
-    val jars: Array[String]= result.split("\n")
+    val jars: Array[String] = result.split("\n")
     jars.mkString(",")
   }
 
@@ -43,12 +45,32 @@ object playerPortfolios {
     val conf = new SparkConf()
     conf.setMaster(master)
     conf.setAppName("Player Portfolio Optimization")
-    val jars = getHiveJars()
-    println(jars)
-    conf.set("spark.driver.extraClassPath", jars)
-    conf.set("spark.executor.extraClassPath", jars)
+    MyKryoRegistrator.register(conf)
     conf
   }
+
+  def playerStatsWhoScoredIn2014(playerSeasonStats: RDD[((String, Int),
+    SingleYearStats)]): RDD[PlayerStats] = {
+    val seasonStatsByPlayersId: RDD[(String, (Int, SingleYearStats))] = playerSeasonStats.map[(String, (Int,
+      SingleYearStats))](record => (record._1._1, (record._1._2,
+      record._2)))
+
+
+    val groupedSeasonStatsByPlayersId: RDD[(String, Map[Int, SingleYearStats])] =
+      seasonStatsByPlayersId.groupByKey().mapValues { statsByYear: Iterable[(Int,
+        SingleYearStats)] => statsByYear.toMap
+      }
+
+    val scoringPlayersOf2014 = groupedSeasonStatsByPlayersId.filter(played2014(_))
+    scoringPlayersOf2014.map[PlayerStats]{record=> PlayerStats.newBuilder().setYear(record._2.)}
+
+  }
+
+  def played2014(record: (String, Map[Int, SingleYearStats])): Boolean = {
+    val playerRecord2014 = record._2(2014)
+    playerRecord2014.totalGamesPlayed != 0 && playerRecord2014.totalStats.mean != 0
+  }
+
 
   /** Entry point to this Spark job. */
   def main(args: Array[String]) {
@@ -61,8 +83,6 @@ object playerPortfolios {
     val gameSeason = DataSource.gamesSeasonHiveSparkSql(sc)
     val playerGame = DataSource.playerGameRddSparkSql(sc)
 
-    playerSeasonStats(playerGame, gameSeason). map(rec =>
-      s"${rec._1._1},${rec._1._2},"
-    )
+    val statsByPlayerSeason = playerSeasonStats(playerGame, gameSeason)
   }
 }
--- src/test/scala/com/cloudera/ds/TestPlayerPortfolio.scala
+++ src/test/scala/com/cloudera/ds/TestPlayerPortfolio.scala
@@ -12,14 +12,14 @@ class TestPlayerPortfolio extends SparkTestUtils with ShouldMatchers {
       new PlayerGameRecord("foo", 1, 0, 1, 3),
       new PlayerGameRecord("bar", 2, 0, 2, 1)))
     val gameSeasonRdd = sc.parallelize(List((1, 2001), (2, 2002)))
-    val pairs: scala.collection.Map[(String, Int), PlayerStats] = playerPortfolios
+    val pairs: scala.collection.Map[(String, Int), SingleYearStats] = PlayerPortfolios
       .playerSeasonStats(playerGameRdd, gameSeasonRdd).collectAsMap()
     pairs should have size 2
-    pairs should contain key (("foo", 2001L))
-    pairs(("foo", 2001)) should equal(PlayerStats(2, StatSummary(0, 0), StatSummary(1, 0),
+    pairs should contain key (("foo", 2001))
+    pairs(("foo", 2001)) should equal(SingleYearStats(2, StatSummary(0, 0), StatSummary(1, 0),
       StatSummary(2, 2), StatSummary(3, 2)))
-    pairs should contain key (("bar", 2002L))
-    pairs(("bar", 2002)) should equal(PlayerStats(1, StatSummary(0, 0), StatSummary(2, 0),
+    pairs should contain key (("bar", 2002))
+    pairs(("bar", 2002)) should equal(SingleYearStats(1, StatSummary(0, 0), StatSummary(2, 0),
       StatSummary(1, 0), StatSummary(3, 0)))
   }
 }
