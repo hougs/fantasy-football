5cf3720bd3c9af09278f0cce2cd548974636c31b
Juliet Hougland <juliet@cloudera.com>
2015-02-09 08:41:53 -0800

    Correctly ID main class.

--- scripts/submit-cmds.sh
+++ scripts/submit-cmds.sh
@@ -8,6 +8,6 @@ HIVE_CLASSPATH=$(find /opt/cloudera/parcels/CDH-5.3.0-1.cdh5.3.0.p0.30/lib/hive/
 export HADOOP_CONF_DIR=/etc/hive/conf
 
 #And use a special command line option for setting the driver classpath
-spark-submit --class com.cloudera.ds.playerPortfolios --master yarn-client \
+spark-submit --class com.cloudera.ds.PlayerPortfolios --master yarn-client \
 --driver-class-path $HIVE_CLASSPATH --conf spark.executor.extraClassPath=$HIVE_CLASSPATH \
 /home/juliet/fantasy-football-0.0.1-SNAPSHOT.jar
